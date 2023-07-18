26956067248cee32ee3d841101ee74d65dbb91de
RIYAZ MOHAMED S <riyazmohamed441@gmail.com>
2023-07-18 23:45:20 +0530

    table model and migration created

---
+++ database/migrations/2023_07_18_172455_create_photos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('userid');
+            $table->binary('photo');
+            $table->string('caption');
+            $table->tinyInteger('deleted')->default(0);
+            $table->timestamps();
+
+            // foreign key code
+            $table->foreign('userid')->references('id')->on('signups')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('photos');
+    }
+};
