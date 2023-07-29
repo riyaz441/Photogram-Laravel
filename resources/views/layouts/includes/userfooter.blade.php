1ea7640084438545bfbbc8bd39e276b9ec506469
RIYAZ MOHAMED S <riyazmohamed441@gmail.com>
2023-07-29 23:13:09 +0530

    user home page template work completed

---
+++ resources/views/layouts/includes/userfooter.blade.php
@@ -0,0 +1,8 @@
+    <footer class="text-body-secondary py-5">
+        <div class="container">
+            <p class="float-end mb-1">
+                <a href="#"><button type="button" class="btn">Back to top</button></a>
+            </p>
+            <p class="mb-1">&copy; Photogram</p>
+        </div>
+    </footer>
