1569ddd5d73146566d075c2d5b984a17693655eb
RIYAZ MOHAMED S <riyazmohamed441@gmail.com>
2023-11-19 13:23:38 +0530

    google button color changes completed

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -212,7 +212,7 @@
                                 new account</a></p>
 
                         <div class="row">
-                            <a class="btn btn-outline-dark" href="{{ route('google-auth') }}" role="button"
+                            <a class="btn btn-outline-primary" href="{{ route('google-auth') }}" role="button"
                                 style="text-transform:none">
                                 <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in"
                                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
