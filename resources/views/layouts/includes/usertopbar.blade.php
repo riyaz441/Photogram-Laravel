a282a871d94c8ea5cf80695b8a905e1b19edbed6
RIYAZ MOHAMED S <riyazmohamed441@gmail.com>
2024-02-04 20:58:25 +0530

    search box missing in mobile view bug fixed

--- resources/views/layouts/includes/usertopbar.blade.php
+++ resources/views/layouts/includes/usertopbar.blade.php
@@ -38,28 +38,6 @@
                 </div>
             </div>
 
-            <!-- Navbar Search-->
-            <form action="/search" method="post" id="search"
-                class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                <div class="input-group p-3">
-                    <div class="input-group input-group-lg">
-                        <span class="input-group-text" id="inputGroup-sizing-lg"><svg xmlns="http://www.w3.org/2000/svg"
-                                width="16" height="16" fill="currentColor" class="bi bi-search"
-                                viewBox="0 0 16 16">
-                                <path
-                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                            </svg></span>
-                        <input type="text" name="search" id="searchbox" placeholder="Search for..."
-                            class="form-control" aria-label="Sizing example input"
-                            aria-describedby="inputGroup-sizing-lg" autocomplete="off">
-                    </div>
-                    {{-- <input class="form-control" type="text" name="search" id="searchbox" placeholder="Search for..."
-                        aria-label="Search for..." aria-describedby="btnNavbarSearch" autocomplete="off" /> --}}
-                    <ul id="searchResults"></ul>
-                </div>
-                @csrf
-            </form>
-
         </div>
     </div>
     <div class="navbar navbar-dark bg-info-subtle shadow-sm">
@@ -73,6 +51,32 @@
                 </svg>
                 <strong>Photogram</strong>
             </a>
+
+            <div class="text-center">
+                <!-- Navbar Search-->
+                <form action="/search" method="post" id="search"
+                    class="form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+                    <div class="input-group">
+                        <div class="input-group input-group-sm">
+                            <span class="input-group-text" id="inputGroup-sizing-lg"><svg
+                                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-search" viewBox="0 0 16 16">
+                                    <path
+                                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                                </svg></span>
+                            <input type="text" name="search" id="searchbox" placeholder="Search for..."
+                                class="form-control" aria-label="Sizing example input"
+                                aria-describedby="inputGroup-sizing-lg" autocomplete="off">
+                        </div>
+                        {{-- <input class="form-control" type="text" name="search" id="searchbox" placeholder="Search for..."
+                        aria-label="Search for..." aria-describedby="btnNavbarSearch" autocomplete="off" /> --}}
+                        <ul id="searchResults"></ul>
+                    </div>
+                    @csrf
+                </form>
+            </div>
+
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
                 aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
